837be77fa08b720e6ed8e929071c1b1611770d38
Sofronijev <sofronijev@gmail.com>
2020-02-17 14:29:06 +0100

    Updates

---
+++ precache-manifest.aec25f383b3681ac4b9f4222fe585271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b67ec1b97014c4bfb971ad28df3e10",
+    "url": "/React-drum-machine/index.html"
+  },
+  {
+    "revision": "3651cf2ef27443e551e2",
+    "url": "/React-drum-machine/static/css/main.e4e72771.chunk.css"
+  },
+  {
+    "revision": "62e9e5bdbfecb5a545c2",
+    "url": "/React-drum-machine/static/js/2.ecdf1d8f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-drum-machine/static/js/2.ecdf1d8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3651cf2ef27443e551e2",
+    "url": "/React-drum-machine/static/js/main.eabd3f96.chunk.js"
+  },
+  {
+    "revision": "4c199bc5a13c72c313df",
+    "url": "/React-drum-machine/static/js/runtime-main.126d5d87.js"
+  }
+]);
